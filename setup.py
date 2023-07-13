a586cdc84e90449f78c28da5aff6d10981dae887
SegawaTenta <72077055+SegawaTenta@users.noreply.github.com>
2023-07-13 19:40:05 +0900

    up

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='DNAMarkMaker',
       description='DNAMarkMaker: pipline to develop ARMS and CAPS marker',
       author_name='Tenta Segawa',
       url='https://github.com/SegawaTenta/DNAMarkMaker-CUI',
-      package=['DNAMarkMaker'],
-      entry_points={'console_scripts':['DNAMarkMaker=dnamarkmaker_script.DNAMarkMaker:main']}
+      packages=['dnamarkmaker_script'],
+      install_requires=["primer3-py"],
+      entry_points={'console_scripts':['DNAMarkMaker=dnamarkmaker_script.DNAMarkMaker:DNAMarkMaker']}
       )
